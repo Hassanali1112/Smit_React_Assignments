6e0977009d8c74cef8af8273f956edab3f70c2ba
hassan ali <hassanali0111272@gmail.com>
2025-03-06 21:35:07 +0500

    Brand container added

--- 01-display-products/src/App.js
+++ 01-display-products/src/App.js
@@ -1,12 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import MyNavbar from "./components/Navbar/MyNavbar";
+import Brands from "./components/Brands/Brands";
 import MyCarousel from "./components/Carousel/MyCarousel";
 
 function App() {
   return (
     <>
     <MyNavbar />
+    <Brands />
     <MyCarousel />
     
       
---
+++ 01-display-products/src/components/Brands/Brands.jsx
@@ -0,0 +1,12 @@
+
+
+const Brands = () =>{
+
+  return (
+    <div className="container px-3">
+      <div className="parent border border-danger py-4"></div>
+    </div>
+  );
+}
+
+export default Brands;
